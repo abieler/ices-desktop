06831c04263302ce43ed9f40f5829c98556a9bfd
abieler <abieler@umich.edu>
2016-02-15 15:22:01 -0500

    new install.jl file to take care of missing julia libraries and the .juliarc.jl file

---
+++ install.jl
@@ -0,0 +1,29 @@
+Pkg.add("Requests")
+Pkg.add("DataFrames")
+Pkg.add("HDF5")
+Pkg.add("JLD")
+Pkg.add("PyPlot")
+Pkg.update()
+
+
+if !isfile(joinpath(homedir(), ".juliarc.jl"))
+  fid = open(joinpath(homedir(), ".juliarc.jl"), "w")
+  write(fid, "push!(LOAD_PATH, pwd())\n")
+  close(fid)
+else
+  load_path = false
+  fid = open(joinpath(homedir(), ".juliarc.jl"), "r")
+  while !eof(fid)
+    line = readline(fid)
+    if contains(line, "LOAD_PATH, pwd())")
+      load_path = true
+    end
+  end
+  close(fid)
+
+  if load_path == false
+    fid = open(joinpath(homedir(), ".juliarc.jl"), "a")
+    write(fid, "push!(LOAD_PATH, pwd())\n")
+    close(fid)
+  end
+end
